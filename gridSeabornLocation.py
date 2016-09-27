5fdf150f6931f5a62bc12ca0045d2c6dbfdec318
aringler-usgs <aringler@usgs.gov>
2016-09-27 11:58:13 -0600

    Added the Location plot

--- gridSeabornLocation.py
+++ gridSeabornLocation.py
@@ -2,18 +2,19 @@
 ###############################################################
 # Intra-Sensor Coherence plots for Max Rohde's Long-Period Tilt Paper
 # Written by Max Rohde and Adam Ringler
+# Additional code for the location was provided by Emily Wolin
 
 import pickle
 import sys
 import numpy as np
-
+import matplotlib.cm as cm
 from scipy import signal
 from obspy.core import UTCDateTime, read, Stream
 
-from matplotlib.pyplot import (figure,xlabel,ylabel,title,subplot,legend,savefig)
+import matplotlib.pyplot as plt
 import matplotlib
 from matplotlib import pylab
-from matplotlib import pyplot
+
 import seaborn as sns
 
 sns.set(font_scale=2.5)
@@ -23,6 +24,7 @@ font = {'family' : 'sans-serif',
         'size'   : 16}
 
 matplotlib.rc('font', **font)
+matplotlib.rc('axes',edgecolor='black')
 
 
 def choptocommon(stream):
@@ -58,6 +60,11 @@ def choptocommon(stream):
         print(stream)
     return stream        
         
+# In meters E/W first, N/S second E,N = 0,0
+distance = [([0.5,0.],[.5,.125],[.5,.25],[.5,.325],[.5,.4],[.5,.525],[.5,.65],[.5,.725]),
+            ([0.,.5],[.125,.5],[.25,.5],[.325,.5],[.4,.5],[.525,.5],[.65,.5],[.725,.5]),
+            ([.2,.1],[.4,.4],[.8,.9],[.6,.4],[.8,.1],[.6,.6],[.4,.6],[.1,.8]),
+            ([.7,.4],[.8,.4],[.9,.4],[.8,.5],[.9,.5],[.7,.6],[.8,.6],[.9,.6])]
 
 
 
@@ -84,7 +91,7 @@ locs = ['00','10']
 
 for idx, day_entry in enumerate(day_list):
     array_number = idx + 1
-
+    print('On array: ' + str(array_number))
     for direction in direction_list:
 
         day = day_entry[0]
@@ -114,17 +121,9 @@ for idx, day_entry in enumerate(day_list):
             coh_stds = []
             for n in range(0, 8):
                 for m in range(0, 8):            
-
-                    subplot(111)
                     x = st[n].data
                     y = st[m].data
                     f, Cxy = signal.coherence(x, y, fs, nperseg=length,noverlap=overlap)
-                    xlabel('Period [s]')
-                    ylabel('Coherence')            
-
-                    
-                    matplotlib.pyplot.close('all')            
-
                     minper = period_ranges[0]
                     maxper = period_ranges[1]
                     per = 1.0/f
@@ -132,36 +131,47 @@ for idx, day_entry in enumerate(day_list):
                     micper = per[(minper <= per) & (per <= maxper)]
                     coh_mean = np.mean(mic_coh)
                     coh_std = np.std(mic_coh)            
-
                     coh_means.append(coh_mean)
-                    coh_stds.append(coh_std)            
+                    coh_stds.append(coh_std)
+            # We now have our array of coherences as well as distances
+            print(distance[idx])
+            x=[]
+            y=[]
+            for ele in distance[idx]:
+                x.append(ele[0]*9)
+                y.append(ele[1]*9)
+            fig = plt.figure(figsize=(10,5))
+            for n in range(0,8):
+                sp = fig.add_subplot(2,4,n+1, aspect='equal')
+                cohs = np.array(coh_means[n*8:(n+1)*8])
+                s= plt.scatter(x, y, c=cohs, cmap=cm.magma, marker='s', vmin=0.,
+                        vmax=1., s=100*cohs**2, linewidth=2)
+                
+                plt.scatter(x[n], y[n], c=1., cmap=cm.magma, marker='s', 
+                    vmin=0., vmax=1., s=100*cohs[n]**2, linewidth=2, edgecolor='k')
+                ax = plt.gca()
+                ax.set_axis_bgcolor('white')
+                sp.set_xlim(-1.1,8.9)
+                sp.set_ylim(-1.1,8.9)
+                plt.axvline(x=-1.1, linewidth=1, color='k')
+                plt.axvline(x=8.9, linewidth=2, color='k')
+                plt.axhline(y=8.9, linewidth=1, color='k')
+                plt.axhline(y=-1.1, linewidth=2, color='k')
+                sp.set_title('Coh. with sen. {0}'.format(n+1), fontsize=14,fontweight='bold')
+                plt.xticks([])
+                plt.yticks([])
 
-            grid_plots = figure()    
-            titlelegend = 'BH' + str(direction)+ ' Array ' + str(array_number) +' ' + str(int(period_ranges[0])) + ' s '+ 'to ' + str(int(period_ranges[1])) + ' s'
-            title(titlelegend,fontsize=24)
-            my_matrix = np.matrix([coh_means[0:8*1:1],coh_means[8*1:8*2:1],coh_means[8*2:8*3:1],coh_means[8*3:8*4:1],coh_means[8*4:8*5:1],coh_means[8*5:8*6:1],coh_means[8*6:8*7:1],coh_means[8*7:8*8:1]])        
-        
+            fig.subplots_adjust(right=0.8)
+            cbar_ax=fig.add_axes([.85, 0.15, 0.05, 0.7])    
+            cbar = fig.colorbar(s, ticks=[0, .25, .5, .75, 1], cax=cbar_ax)
+            for l in cbar.ax.yaxis.get_ticklabels():
+                l.set_weight("bold")
+                l.set_fontsize(14)
+            fig.suptitle('Coherence Array ' + str(array_number) + ' BH' + str(direction) + ' ' + str(int(minper)) + ' s to ' + str(int(maxper)) + ' s Period' , fontsize=16, fontweight='bold')    
 
-            ax = grid_plots.add_subplot(1,1,1)        
-
-            names = range(1, 9)
-            heat= sns.heatmap(my_matrix, vmin=0, vmax=1,cmap = pylab.cm.magma,linewidths=.5,annot = True, xticklabels= names, yticklabels = names)
-            ylabel('Sensor Number',fontsize=22)    
-            xlabel('Sensor Number',fontsize=22)
-            pylab.yticks(rotation=0)        
-            #savefig("Day"+str(day)+'_GRID_Plot_BH'+ str(direction)+'_' + '_from_' + (str(start_time)[:-11]).replace('/',':') + '_to_'+ (str(end_time)[:-11]).replace('/',':') +'_'+ str(period_ranges[0]) + 's' + str(period_ranges[1]) + 's'+'.eps',format='eps',dpi=1000,bbox_inches='tight')
-            savefig("Day"+str(day)+'_GRID_Plot_BH'+ str(direction)+'_' + '_from_' + (str(start_time)[:-11]).replace('/',':') + '_to_'+ (str(end_time)[:-11]).replace('/',':') +'_'+ str(period_ranges[0]) + 's' + str(period_ranges[1]) + 's'+'.jpg',format='jpeg',dpi=400,bbox_inches='tight')
-        
-
-            matplotlib.pyplot.close('all')        
-            sys.exit()
-        
-        
-
-
-
-
-
+            fig.savefig("WolinPltDay"+str(day)+'_GRID_Plot_BH'+ str(direction) + '_from_' + (str(start_time)[:-11]).replace(':','') + '_to_'+ (str(end_time)[:-11]).replace(':','') +'_'+ str(int(period_ranges[0])) + '_' + str(int(period_ranges[1])) +'.jpg',format='jpeg',dpi=400,bbox_inches='tight')
+            fig.savefig("WolinPltDay"+str(day)+'_GRID_Plot_BH'+ str(direction) + '_from_' + (str(start_time)[:-11]).replace(':','') + '_to_'+ (str(end_time)[:-11]).replace(':','') +'_'+ str(int(period_ranges[0])) + '_' + str(int(period_ranges[1])) +'.eps',format='eps',dpi=800,bbox_inches='tight')
+            plt.close()
 
 
 
